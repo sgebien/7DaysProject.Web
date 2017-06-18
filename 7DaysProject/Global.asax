a17d71dede8448319f0a2a5182d1e9022c76b61c
sg <sg@WSF88>
2017-06-18 10:33:40 +0200

    Add project files.

---
+++ 7DaysProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_7DaysProject.MvcApplication" Language="C#" %>
---
+++ 7DaysProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_7DaysProject.Startup))]
+namespace _7DaysProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
